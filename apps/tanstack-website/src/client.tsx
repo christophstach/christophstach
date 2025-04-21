86441f791435998d175e2b957aac9ddcf6e1ea4a
Christoph Stach <christoph.stach@gmail.com>
2025-04-21 11:27:21 +0200

    chore: remove unused uno.css import from client.tsx and add it to ssr.tsx

--- apps/tanstack-website/src/client.tsx
+++ apps/tanstack-website/src/client.tsx
@@ -2,7 +2,6 @@ import { StartClient } from '@tanstack/react-start'
 /// <reference types="vinxi/types/client" />
 import { hydrateRoot } from 'react-dom/client'
 import { createRouter } from './router'
-import 'virtual:uno.css'
 
 const router = createRouter()
 
